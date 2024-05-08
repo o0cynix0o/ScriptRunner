70f4e9754282e949d6779274fa39d1838af73ec6
o0cynix0o <o0cynix0o@gmail.com>
2024-05-08 08:43:49 -0600

    Create ScriptRunner.ps1

---
+++ ScriptRunner.ps1
@@ -0,0 +1,21 @@
+# Create a GUI with a button
+
+Add-Type -AssemblyName System.Windows.Forms
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Script Runner"
+$form.Size = New-Object System.Drawing.Size(300,200)
+
+$button = New-Object System.Windows.Forms.Button
+$button.Location = New-Object System.Drawing.Point(75,50)
+$button.Size = New-Object System.Drawing.Size(150,50)
+$button.Text = "Run Script"
+$button.Add_Click({
+    # Specify the path to the script you want to run
+    $scriptPath = "C:\Path\to\your\script.ps1"
+    # Run the script
+    & $scriptPath
+})
+
+$form.Controls.Add($button)
+
+$form.ShowDialog() | Out-Null
